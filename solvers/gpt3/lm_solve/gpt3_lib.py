82607367d7973c03f43691fe987b01e3ef7a0533
theroboticrooster <theroboticrooster@gmail.com>
2022-04-19 15:42:52 -0400

    test

--- solvers/gpt3/lm_solve/gpt3_lib.py
+++ solvers/gpt3/lm_solve/gpt3_lib.py
@@ -53,97 +53,119 @@ def _load_cache():
 
 
 
+
 def query(prompt, n=10, max_tokens=150, temp=1.0, max_batch=32, stop=None, notes=None, cache_only=False, verbose=True):
-    """Query gpt3
-
-    :param prompt: Up to 2048 tokens (about 3-4k chars)
-    :param n: number of answers, None returns all cached answers
-    :param max_tokens:
-    :param temp: 0.9 seems to work well
-    :param max_batch: max to query at once
-    :param stop: string to stop at or '' if not to stop
-    :param notes: notes you want to save or change in case you want to run the same query more than once!
-    :return: list of answers and then the response items
-    """
-    global _cache
-    if _cache is None:
-        _load_cache()
-
-    if temp == 0 and n > 1:
-        ezlog.debug("Temp 0: no point in running more than one query")
-        n = 1
-
-    key = str(dict(prompt=prompt, max_tokens=max_tokens, temp=temp, max_batch=max_batch, stop=stop, rep=notes))
-    cached = _cache.get(key, [])
-    if n is None:
-        return cached[:]
-
-    if len(cached) >= n:
-        return cached[:n]
-
-    if cache_only:
-        pass
-        1/0
-
-    assert not cache_only, "Entry not found in cache"
     if verbose:
         print("/"*100)
-        print("Querying GPT3 with prompt:")
+        print("Querying GPT-Neo with prompt:")
         print(prompt)
         s = stop and stop.replace('\n', '\\n')
         print(f"/// n={n} ({n-len(cached)} new) max_tokens={max_tokens} temp={temp} max_batch={max_batch} stop={s}")
         print("/"*100)
 
-    time0 = time.perf_counter()
-
-    new = []
-    n -= len(cached)
-
     while n > 0:
-        m = min(n, max_batch)
-
-        # res = openai.Completion.create(
-        #     engine="davinci-msft",
-        #     prompt=prompt,
-        #     max_tokens=max_tokens,
-        #     temperature=temp,
-        #     n=m,
-        #     stop=stop or None
-        # )
-        # new += [c["text"] for c in res["choices"]]
-
-        # input_ids = tokenizer(prompt, return_tensors="pt").input_ids
-
-        # res_tokens = model.generate(
-        #     inputs = input_ids,
-        #     max_new_tokens = max_tokens,
-        #     temperature = temp,
-        #     num_return_sequences = m,
-        #     do_sample=True
-        # )
-
-        # res = tokenizer.batch_decode(res_tokens)
-
         res = generator(
             text_inputs = prompt,
-            max_length = max_tokens,
-            num_return_sequences = m,
-            return_full_text=False,
-            do_sample=True
+            max_length = 550,
+            num_return_sequences = 4,
+            return_full_text=False
         )
-
-
-        # new += list(dict.fromkeys([c['generated_text'] for c in res]))
+        print(list(dict.fromkeys([c['generated_text'] for c in res])))
         new += [c['generated_text'] for c in res]
+        n -= 1
+    return new
 
-        # print(new)
+# def query(prompt, n=10, max_tokens=150, temp=1.0, max_batch=32, stop=None, notes=None, cache_only=False, verbose=True):
+#     """Query gpt3
 
-        n -= m
+#     :param prompt: Up to 2048 tokens (about 3-4k chars)
+#     :param n: number of answers, None returns all cached answers
+#     :param max_tokens:
+#     :param temp: 0.9 seems to work well
+#     :param max_batch: max to query at once
+#     :param stop: string to stop at or '' if not to stop
+#     :param notes: notes you want to save or change in case you want to run the same query more than once!
+#     :return: list of answers and then the response items
+#     """
+#     global _cache
+#     if _cache is None:
+#         _load_cache()
 
-    _save_line((key, new), f"{time.perf_counter() - time0:.1f}s {datetime.datetime.now()}")
-    ans = _cache[key] = cached + new
-    print(ans)
-    return ans[:]
+#     if temp == 0 and n > 1:
+#         ezlog.debug("Temp 0: no point in running more than one query")
+#         n = 1
+
+#     key = str(dict(prompt=prompt, max_tokens=max_tokens, temp=temp, max_batch=max_batch, stop=stop, rep=notes))
+#     cached = _cache.get(key, [])
+#     if n is None:
+#         return cached[:]
+
+#     if len(cached) >= n:
+#         return cached[:n]
+
+#     if cache_only:
+#         pass
+#         1/0
+
+#     assert not cache_only, "Entry not found in cache"
+#     if verbose:
+#         print("/"*100)
+#         print("Querying GPT3 with prompt:")
+#         print(prompt)
+#         s = stop and stop.replace('\n', '\\n')
+#         print(f"/// n={n} ({n-len(cached)} new) max_tokens={max_tokens} temp={temp} max_batch={max_batch} stop={s}")
+#         print("/"*100)
+
+#     time0 = time.perf_counter()
+
+#     new = []
+#     n -= len(cached)
+
+#     while n > 0:
+#         m = min(n, max_batch)
+
+#         # res = openai.Completion.create(
+#         #     engine="davinci-msft",
+#         #     prompt=prompt,
+#         #     max_tokens=max_tokens,
+#         #     temperature=temp,
+#         #     n=m,
+#         #     stop=stop or None
+#         # )
+#         # new += [c["text"] for c in res["choices"]]
+
+#         # input_ids = tokenizer(prompt, return_tensors="pt").input_ids
+
+#         # res_tokens = model.generate(
+#         #     inputs = input_ids,
+#         #     max_new_tokens = max_tokens,
+#         #     temperature = temp,
+#         #     num_return_sequences = m,
+#         #     do_sample=True
+#         # )
+
+#         # res = tokenizer.batch_decode(res_tokens)
+
+#         res = generator(
+#             text_inputs = prompt,
+#             max_length = max_tokens,
+#             num_return_sequences = m,
+#             return_full_text=False,
+#             do_sample=True
+#         )
+
+
+#         # new += list(dict.fromkeys([c['generated_text'] for c in res]))
+#         new += [c['generated_text'] for c in res]
+
+#         # print(new)
+
+#         n -= m
+
+#     _save_line((key, new), f"{time.perf_counter() - time0:.1f}s {datetime.datetime.now()}")
+#     ans = _cache[key] = cached + new
+#     print(ans)
+#     return ans[:]
 
 # old code
 # # to persist calls to the API...
