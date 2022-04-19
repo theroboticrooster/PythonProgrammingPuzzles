c130fc2cf150256190a291b4e38f260387ae9d01
theroboticrooster <theroboticrooster@gmail.com>
2022-04-18 20:51:47 -0400

    test

--- solvers/gpt3/lm_solve/gpt3_lib.py
+++ solvers/gpt3/lm_solve/gpt3_lib.py
@@ -111,7 +111,7 @@ def query(prompt, n=10, max_tokens=150, temp=1.0, max_batch=32, stop=None, notes
         print(prompt)
         # print(prompt)
 
-        # input_ids = tokenizer(prompt, return_tensors="pt").input_ids
+        input_ids = tokenizer(prompt, return_tensors="pt").input_ids
 
 
         # res = openai.Completion.create(
